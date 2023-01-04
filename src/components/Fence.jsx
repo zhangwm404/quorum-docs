5e77d65814cf908cf61d9c0f24e1317654e736bc
okdaodine <btc.link.grt@gmail.com>
2023-01-04 18:27:29 +0800

    add copy button to code

--- src/components/Fence.jsx
+++ src/components/Fence.jsx
@@ -1,7 +1,20 @@
-import { Fragment } from 'react'
-import Highlight, { defaultProps } from 'prism-react-renderer'
+import * as React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import copy from 'copy-to-clipboard';
+import { MdContentCopy, MdCheck } from 'react-icons/md';
+import clsx from 'clsx'
 
 export function Fence({ children, language }) {
+  const [copied, setCopied] = React.useState(false);
+
+  React.useEffect(() => {
+    if (copied) {
+      copy(children.trimEnd());
+      const to = setTimeout(setCopied, 1000, false);
+      return () => clearTimeout(to);
+    }
+  }, [copied, children]);
+
   return (
     <Highlight
       {...defaultProps}
@@ -10,15 +23,18 @@ export function Fence({ children, language }) {
       theme={undefined}
     >
       {({ className, style, tokens, getTokenProps }) => (
-        <pre className={className} style={style}>
+        <pre className={clsx(className, 'relative')} style={style}>
+          <div onClick={() => setCopied(true)} className="absolute top-1 right-1 opacity-70 cursor-pointer text-[16px] p-2">
+            {copied ? <MdCheck /> : <MdContentCopy />}
+          </div>
           <code>
             {tokens.map((line, index) => (
-              <Fragment key={index}>
+              <React.Fragment key={index}>
                 {line.map((token, index) => (
                   <span key={index} {...getTokenProps({ token })} />
                 ))}
                 {'\n'}
-              </Fragment>
+              </React.Fragment>
             ))}
           </code>
         </pre>
